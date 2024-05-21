e8116eb936f83ee7712dd24400f37cced3f4d146
AureMurillo <amurillo@bankaool.com>
2024-05-20 21:08:06 -0600

    all

--- Lector/obj/Debug/net6.0/Lector.AssemblyInfo.cs
+++ Lector/obj/Debug/net6.0/Lector.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad033058104449e618021a4f1ee1d78e7752465")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
